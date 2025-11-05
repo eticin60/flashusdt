ec7c51521bb07ded9531be206f2c15a0d898e99b
eticin60 <95057162+eticin60@users.noreply.github.com>
2025-11-05 15:39:16 +0300

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("flashusdt-store").then(cache => {
+      return cache.addAll(["/", "/index.html", "/assets/USDT-2.png"]);
+    })
+  );
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
